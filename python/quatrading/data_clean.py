7cadcc750e4a95c8be9cac15f9a5df8a43a4aab9
zx-176 <wgf5544@163.com>
2019-05-22 11:05:18 +0800

    xxx

--- python/quatrading/data_clean.py
+++ python/quatrading/data_clean.py
@@ -38,13 +38,42 @@ def view_info(df: DataFrame) -> str:
 
     return None
 
-"""
-缺失值处理
-数据有可能出现一些缺失值NaN(NOT a NUMBER)
-"""
+
 if __name__ == "__main__":
     df_csvload = pd.read_csv('../csv/table_stock', parse_dates=True, index_col=0, encoding='gb2312')
     # 数据信息查看
     df_info = view_info(df_csvload)
     print(df_info)
-    print(df_csvload.isnull())
+
+    """
+    缺失值处理
+    数据有可能出现一些缺失值NaN(NOT a NUMBER)
+    """
+    print(df_csvload.isnull())  # 缺失为True
+    print(df_csvload.notnull())  # 缺失为False
+    print("--------------------")
+
+    # 查找缺失值所在的行
+    print(df_csvload[df_csvload.isnull().T.any().T])  # .any()筛选满足True值条件列方法,查找出含有NaN的行。
+
+    # 缺失值删除
+    '''
+    axis=0表示删除行，=1表示删除列；how='any'表示只要有一个缺失值就删除,='all'表示当只有所有值为缺失值才删除。
+    '''
+
+    df_csvload_del = df_csvload.dropna(axis=0, how='any')
+    print(df_csvload_del)
+    # 缺失值填充
+    '''
+    method是填充方式，='ffill'表示用行或列上一个值来填充缺失值，='bfill'表示用下一个值来填充，inplace=True表示原地修改。
+    axis=0表示列，=1表示行。
+    '''
+    df_csvload.fillna(method='bfill', axis=0, inplace=True)
+    print('填充缺失值-------------------------------------')
+    print(df_csvload)
+    print(df_csvload[df_csvload.isnull().values == True])  # 查看删除和填充缺失值后的值
+
+
+    '''
+    特殊值处理
+    '''
